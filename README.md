4f9c3feb9c13fcf6ecad25bcb5df5c8f026d02bb
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-05-03 16:37:22 +0900

    KETI_pre_data_ingestion

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # data_influx
 
-- influx information: get influx db information
-- db information: get db information by influx db name
-- measurement_ingestion: data ingestion from multiple sources such as influxDB, and CSV files
+- info_influx: get influx db information
+- info_db: get db information by influx db name
+- ingestion_measurement: data ingestion from multiple sources such as influxDB, and CSV files
 
 
+# data_document
+- data_csv : get data from csv by local address
+- data_json: get data from json by local address
+
+# KETI_setting
+
+Store the local setting information
+- influx_setting_KETI: KETI influx DB information
--- data_influx_test.py
+++ data_influx_test.py
@@ -6,20 +6,11 @@ from KETI_setting import influx_setting_KETI as ins
 from data_influx import measurement_ingestion as ing
 
        
-def test1():
-    #from . KETI_setting import influx_setting_KETI as ins
-    #dbnames=['INNER_AIR','OUTDOOR_AIR','OUTDOOR_WEATHER']
-    #measurement=['KDS1','KDS2','HS1','HS2','sangju']
+def test1(db_name, measurement, time_start, time_end):
     
-    time_start='2020-09-10'
-    time_end='2021-09-25'
-    
-    dbname= "INNER_AIR"
-    table = "HS1"
-    
-    print("dbname:",dbname, "table:", table)
-    influx_c = ing.Influx_management(ins.host_, ins.port_, ins.user_, ins.pass_, dbname, ins.protocol)
-    result = influx_c.get_df_by_time(time_start,time_end,table)
+    print("dbname:",db_name, "table:", measurement)
+    influx_c = ing.Influx_management(ins.host_, ins.port_, ins.user_, ins.pass_, db_namedk s, ins.protocol)
+    result = influx_c.get_df_by_time(time_start,time_end,measurement)
     print(result)
 
 
@@ -35,4 +26,16 @@ if __name__ =='__main__':
     
     db_list = test2()
     print(db_list)
-    test1() 
+    
+    
+    #from . KETI_setting import influx_setting_KETI as ins
+    #dbnames=['INNER_AIR','OUTDOOR_AIR','OUTDOOR_WEATHER']
+    #measurement=['KDS1','KDS2','HS1','HS2','sangju']
+
+    time_start='2020-09-10'
+    time_end='2021-09-25'
+    
+    db_name= "INNER_AIR"
+    measurement = "HS1"
+    
+    test1(db_name, measurement, time_start, time_end) 

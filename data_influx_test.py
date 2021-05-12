d618f58c3e7091167b4e8e40329cf4e04824d091
Hwangjisoo <ghkdwltn8@gmail.com>
2021-05-12 15:52:47 +0900

    second_commit

--- data_influx_test.py
+++ data_influx_test.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 import numpy as np
 import pandas as pd
 from influxdb import InfluxDBClient, DataFrameClient

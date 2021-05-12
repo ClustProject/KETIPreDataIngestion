558794b70fe7ba27ba90e6a1b2bffda57bed7188
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-05-12 13:57:02 +0900

    minor

--- data_influx/ingestion_partial_dataset.py
+++ data_influx/ingestion_partial_dataset.py
@@ -5,14 +5,13 @@ def partial_dataSet_ingestion(intDataInfo, influx_parameter):
     
     result={}
     for i, dbinfo in enumerate(intDataInfo['db_info']):
-        print(dbinfo)
         db_name = dbinfo['db_name']
         measurement = dbinfo['measurement']
         ing_start= dbinfo['start']
         ing_end = dbinfo['end']
         influx_c = ing.Influx_management(influx_parameter.host_, influx_parameter.port_, influx_parameter.user_, influx_parameter.pass_, db_name, influx_parameter.protocol)
         result[i] = influx_c.get_df_by_time(ing_start,ing_end,measurement)
-    
+        
     return result
 
 

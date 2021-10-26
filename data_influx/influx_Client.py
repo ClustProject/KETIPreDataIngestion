73deb61b8364c84dbc6bc33a775ca4b73728281f
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-10-26 13:51:17 +0900

    modification

--- data_influx/influx_Client.py
+++ data_influx/influx_Client.py
@@ -23,34 +23,33 @@ class influxClient():
             db_list.append(ori_db_list[num]['name'])
         return db_list
 
-class influxDB():
-    """
-    explore the specific database DB with switching database name.
+##### DB Function
 
-    """
-    def __init__(self, influxClient, db_name):
+    def switch_DB(self, db_name):
         """
-        switch database based on influxClient and the db name
+        Before explore the specific DB, Switch DB.
+
         """
-        self.DBClient = influxClient
         self.db_name = db_name 
-        self.DBClient.switch_database(self.db_name)
+        self.DBClient.switch_database(self.db_name)        
     
-    def measurement_list(self):
+    def measurement_list(self, db_name):
         """
         get all measurement list related to the db
         """
+        self.switch_DB(db_name)
         measurement_list = []
         ori_ms_list=self.DBClient.get_list_measurements()
         for num in range(len(ori_ms_list)):
             measurement_list.append(ori_ms_list[num]['name'])
         return measurement_list
 
-    def measurement_list_only_start_end(self):
+    def measurement_list_only_start_end(self, db_name):
         """
         Get the only start and end measurement name
         Use this function to reduce the DB load time.
         """
+        self.switch_DB(db_name)
         measurement_list = []
         ori_ms_list=self.DBClient.get_list_measurements()
         ori_len = len(ori_ms_list)
@@ -64,80 +63,76 @@ class influxDB():
             measurement_list.append("...(+"+str(ori_len-2)+")")
             measurement_list.append(ori_ms_list[len(ori_ms_list)-1]['name'])
         return measurement_list
+##### MS Set Function
 
-class influxMeasurementSet():
-    """
-    This class get multiple influx measurement information.
-    """
-    def __init__(self, DBClient, intDataInfo):
-        self.DBClient = DBClient
-        self.intDataInfo = intDataInfo
-
-    def get_MeasurementDataSet(self):
+    def get_MeasurementDataSet(self, intDataInfo):
         """
         Get measurement Data Set according to the dbinfo
         Each function makes dataframe output with "timedate" index.
 
         """
         MSdataSet ={}
-        for i, dbinfo in enumerate(self.intDataInfo['db_info']):
+        for i, dbinfo in enumerate(intDataInfo['db_info']):
             db_name = dbinfo['db_name']
             ms_name = dbinfo['measurement']
-            infMS= influxMeasurement(self.DBClient, db_name, ms_name)
+            self.switch_MS(db_name, ms_name)
             bind_params = {'end_time': dbinfo['end'], 'start_time': dbinfo['start']}
-            MSdataSet[i] =infMS.get_data_by_time(bind_params)
+            MSdataSet[i] =self.get_data_by_time(bind_params, db_name, ms_name)
             MSdataSet[i].index.name ='datetime'
 
         return MSdataSet
 
-class influxMeasurement():
-    """
-        Get the specific measurement data and information.
-    """
-    def __init__(self, influxClient, db_name, ms_name):
-        self.DBClient = influxClient
+    ##### MS Function
+    def switch_MS(self, db_name, ms_name):
+        """
+        Before getting the specific measurement data and information, switch MS
+        """
         self.db_name = db_name 
         self.ms_name = ms_name
-        self.DBClient.switch_database(self.db_name)
-
-    def get_fieldList(self):
+        self.DBClient.switch_database(db_name)
+        
+    def get_fieldList(self, db_name, ms_name):
         """
         Get all feature(field) list of the specific measurement.
         """
+        self.switch_MS(db_name, ms_name)
         query_string = "SHOW FIELD KEYS"
-        fieldkeys = list(self.DBClient.query(query_string).get_points(measurement=self.ms_name))
+        fieldkeys = list(self.DBClient.query(query_string).get_points(measurement=ms_name))
         fieldList = list(x['fieldKey'] for x in fieldkeys)
 
         return fieldList
-    
-    def get_first_data(self):
+
+    def get_first_data(self, db_name, ms_name):
         """
         Get the first data of the specific mearuement
         """
-        query_string = 'select * from "'+self.ms_name+''+'" ORDER BY DESC LIMIT 1'
+        self.switch_MS(db_name, ms_name)
+        query_string = 'select * from "'+ms_name+''+'" ORDER BY DESC LIMIT 1'
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df
 
-    def get_last_data(self):
+    def get_last_data(self, db_name, ms_name):
         """
         Get the last data of the specific mearuement
         """
-        query_string = 'select * from "'+self.ms_name+''+'" ORDER BY DESC LIMIT 1'
+        self.switch_MS(db_name, ms_name)
+        query_string = 'select * from "'+ms_name+''+'" ORDER BY DESC LIMIT 1'
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df
 
-    def get_data(self):
+    def get_data(self,db_name, ms_name):
         """
         Get all data of the specific mearuement
         """
-        query_string = "select * from "+self.ms_name+""
+        self.switch_MS(db_name, ms_name)
+        query_string = "select * from "+ms_name+""
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df
 
-    def get_data_by_time(self, bind_params):
+    def get_data_by_time(self, bind_params, db_name, ms_name):
         """
         Get data of the specific mearuement based on start-end duration
         # get_datafront_by_duration(self, start_time, end_time)
@@ -145,39 +140,42 @@ class influxMeasurement():
         bind_params = {'end_time': query_end_time.strftime('%Y-%m-%dT%H:%M:%SZ'), 
         'start_time': query_start_time.strftime('%Y-%m-%dT%H:%M:%SZ')}
         """
-        query_string = "select * from "+self.ms_name+" where time >= $start_time and time < $end_time"
+        self.switch_MS(db_name, ms_name)
+        query_string = "select * from "+ms_name+" where time >= $start_time and time < $end_time"
         df = pd.DataFrame(self.DBClient.query(query_string, bind_params = bind_params).get_points())
         df = self.cleanup_df(df)
         return df
 
-    def get_data_by_days(self, bind_params):
+    def get_data_by_days(self, bind_params, db_name, ms_name):
         """
         Get data of the specific mearuement based on time duration (days)
         
         ex> bind_param example
         bind_params = {'end_time': 1615991400000, 'days': '7d"}
         """
-        query_string = 'select * from "'+self.ms_name+'" where time >= '+bind_params["end_time"]+" - "+bind_params["days"]
+        self.switch_MS(db_name, ms_name)
+        query_string = 'select * from "'+ms_name+'" where time >= '+bind_params["end_time"]+" - "+bind_params["days"]
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df
 
 
-    def get_datafront_by_num(self, number):
+    def get_datafront_by_num(self, number, db_name, ms_name):
         """
         Get the first N number data from the specific measurement
         """
-        query_string = "SELECT * FROM " + self.ms_name +" LIMIT "+ number +""
+        self.switch_MS(db_name, ms_name)
+        query_string = "SELECT * FROM " + ms_name +" LIMIT "+ number +""
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df
 
-    def get_dataend_by_num(self, number):
+    def get_dataend_by_num(self, number, db_name, ms_name):
         """
         Get the last N number data from the specific measurement
         """
-
-        query_string = "SELECT * FROM " + self.ms_name +" ORDER BY DESC LIMIT "+ number +""
+        self.switch_MS(db_name, ms_name)
+        query_string = "SELECT * FROM " + ms_name +" ORDER BY DESC LIMIT "+ number +""
         df = pd.DataFrame(self.DBClient.query(query_string).get_points())
         df = self.cleanup_df(df)
         return df

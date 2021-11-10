a706c7917caae69acd844366513dd9d3dc3bfc80
Mishuni <miseon543@gmail.com>
2021-11-10 17:26:36 +0900

    Update

--- data_influx/influx_Client.py
+++ data_influx/influx_Client.py
@@ -97,7 +97,7 @@ class influxClient():
         """
         self.switch_MS(db_name, ms_name)
         query_string = "SHOW FIELD KEYS"
-        fieldkeys = list(self.DBClient.query(query_string).get_points(measurement='"'+ms_name+'"'))
+        fieldkeys = list(self.DBClient.query(query_string).get_points(measurement=ms_name))
         fieldList = list(x['fieldKey'] for x in fieldkeys)
 
         return fieldList
